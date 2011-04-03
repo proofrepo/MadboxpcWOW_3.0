0d5f328c6d6e95034885e06da5de648422cbd896
Retriman <nicolas_katre@hotmail.com>
2011-04-03 17:21:44 -0400

    [DB/Quests] Fix Quest 9446 NPC's talking draconic

---
+++ sql/Madobox_Update/Quest/[Quest][9446].sql
@@ -0,0 +1,9 @@
+-- DB/Quests Fix Quest 9446 NPC's talking draconic
+-- Fix for Quest 9446 NPC talking Draconic
+UPDATE `script_texts` SET `language`=7 WHERE `npc_entry`=0 AND `entry`=-1800070 LIMIT 1;
+UPDATE `script_texts` SET `language`=7 WHERE `npc_entry`=0 AND `entry`=-1800069 LIMIT 1;
+UPDATE `script_texts` SET `language`=7 WHERE `npc_entry`=0 AND `entry`=-1800068 LIMIT 1;
+UPDATE `script_texts` SET `language`=7 WHERE `npc_entry`=0 AND `entry`=-1800067 LIMIT 1;
+UPDATE `script_texts` SET `language`=7 WHERE `npc_entry`=0 AND `entry`=-1800066 LIMIT 1;
+UPDATE `script_texts` SET `language`=7 WHERE `npc_entry`=0 AND `entry`=-1800065 LIMIT 1;
+UPDATE `script_texts` SET `language`=7 WHERE `npc_entry`=0 AND `entry`=-1800064 LIMIT 1;
