975b3b94afd8da7a29198f3dc972df05311dbc5b
Retriman <nicolas_katre@hotmail.com>
2011-04-03 17:24:56 -0400

    [DB/Quest] Fix Quest 11061 The Plight of a Father

---
+++ sql/Madobox_Update/Quest/[Quest][11061].sql
@@ -0,0 +1,3 @@
+-- DB/Quest Fix Quest 11061 The Plight of a Father
+-- Fix Quest 11061 - The Plight of a Father
+UPDATE gameobject SET position_x=4020.290039, position_y=5895.250000, position_z=267.871002, orientation=3.860500 WHERE guid=13355;
