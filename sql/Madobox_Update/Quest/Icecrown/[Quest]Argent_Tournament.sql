7154c2edbe3c181bb15c401b497f37d3d75b65df
Retriman <nicolas_katre@hotmail.com>
2011-04-03 17:10:16 -0400

    [Core/Script]Argent Tournament: Implement spells mount 62960,62575,62544,62863

---
+++ sql/Madobox_Update/Quest/Icecrown/[Quest]Argent_Tournament.sql
@@ -0,0 +1,7 @@
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (62960,62575,62544,62863);
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
+('62960', 'spell_tournament_charge'),
+('62575', 'spell_tournament_shield'),
+('62544', 'spell_tournament_melee'),
+('62863', 'spell_tournament_duel');
+
--- src/server/scripts/Northrend/icecrown.cpp
+++ src/server/scripts/Northrend/icecrown.cpp
@@ -28,6 +28,8 @@ npc_arete
 EndContentData */
 
 #include "ScriptPCH.h"
+#include "SpellAuraEffects.h"
+#include "ScriptMgr.h"
 
 /*######
 ## npc_arete
@@ -381,6 +383,178 @@ public:
     }
 };
 
+/*######
+## Argent Tournament - Fix
+## Spell Fixed:
+##             - 62960 - Charge Mount Npc.
+##             - 62575 - Shield Mount Npc.
+##             - 62544 - Melee Mount Npc.
+##             - 62863 - Duel Mount Npc.
+######*/
+
+class spell_tournament_charge : public SpellScriptLoader
+{
+    public:
+        spell_tournament_charge() : SpellScriptLoader("spell_tournament_charge") { }
+
+        class spell_tournament_charge_SpellScript : public SpellScript
+        {
+            PrepareSpellScript(spell_tournament_charge_SpellScript);
+
+            void HandleEffectScriptEffect(SpellEffIndex /*effIndex*/)
+            {
+                if (Unit* pTarget = GetHitUnit())
+                {
+                    if (Unit *caster = GetCaster())
+                    {
+                        caster->CastSpell(pTarget,74399,true);
+                        caster->CastSpell(pTarget,68321,true);
+
+                        if (pTarget->GetTypeId() == TYPEID_UNIT && pTarget->ToCreature()->GetEntry() == 33272)
+                        {
+                            //Kill Credit
+                            if (Unit *player = caster->GetCharmerOrOwner())
+                                player->CastSpell(player,62658,true);
+                        }
+                    }
+
+                    if (pTarget->GetAura(64100))
+                        pTarget->RemoveAuraFromStack(64100);
+                    else if (pTarget->GetAura(62552))
+                        pTarget->RemoveAuraFromStack(62552);
+                    else if (pTarget->GetAura(62719))
+                        pTarget->RemoveAuraFromStack(62719);
+                }
+            }
+
+            void Register()
+            {
+                OnEffect += SpellEffectFn(spell_tournament_charge_SpellScript::HandleEffectScriptEffect, EFFECT_0, SPELL_EFFECT_SCRIPT_EFFECT);
+            }
+        };
+
+        SpellScript *GetSpellScript() const
+        {
+            return new spell_tournament_charge_SpellScript();
+        }
+};
+
+class spell_tournament_shield : public SpellScriptLoader
+{
+    public:
+        spell_tournament_shield() : SpellScriptLoader("spell_tournament_shield") { }
+
+        class spell_tournament_shield_SpellScript : public SpellScript
+        {
+            PrepareSpellScript(spell_tournament_shield_SpellScript);
+
+            void HandleEffectScriptEffect(SpellEffIndex /*effIndex*/)
+            {
+                if (Unit* pTarget = GetHitUnit())
+                {
+                    if (Unit *caster = GetCaster())
+                    {
+                        caster->CastSpell(pTarget,62626,true );
+
+                        if (pTarget->GetTypeId() == TYPEID_UNIT && pTarget->ToCreature()->GetEntry() == 33243)
+                        {
+                            //Kill Credit
+                            if (Unit *player = caster->GetCharmerOrOwner())
+                                player->CastSpell(player,62673,true);
+                        }
+                    }
+
+                    if (pTarget->GetAura(64100))
+                        pTarget->RemoveAuraFromStack(64100);
+                    else if (pTarget->GetAura(62552))
+                        pTarget->RemoveAuraFromStack(62552);
+                    else if (pTarget->GetAura(62719))
+                        pTarget->RemoveAuraFromStack(62719);
+                }
+            }
+
+            void Register()
+            {
+                OnEffect += SpellEffectFn(spell_tournament_shield_SpellScript::HandleEffectScriptEffect, EFFECT_0, SPELL_EFFECT_SCRIPT_EFFECT);
+            }
+        };
+
+        SpellScript *GetSpellScript() const
+        {
+            return new spell_tournament_shield_SpellScript();
+        }
+};
+
+class spell_tournament_melee : public SpellScriptLoader
+{
+    public:
+        spell_tournament_melee() : SpellScriptLoader("spell_tournament_melee") { }
+
+        class spell_tournament_melee_SpellScript : public SpellScript
+        {
+            PrepareSpellScript(spell_tournament_melee_SpellScript);
+
+            void HandleEffectScriptEffect(SpellEffIndex /*effIndex*/)
+            {
+                if (Unit* pTarget = GetHitUnit())
+                {
+                    if (Unit *caster = GetCaster())
+                    {
+                        if (pTarget->GetTypeId() == TYPEID_UNIT && pTarget->ToCreature()->GetEntry() == 33229)
+                        {
+                            //Kill Credit
+                            if (Unit *player = caster->GetCharmerOrOwner())
+                                player->CastSpell(player,62672,true);
+                        }
+                    }
+                }
+            }
+
+            void Register()
+            {
+                OnEffect += SpellEffectFn(spell_tournament_melee_SpellScript::HandleEffectScriptEffect, EFFECT_0, SPELL_EFFECT_SCHOOL_DAMAGE);
+            }
+        };
+
+        SpellScript *GetSpellScript() const
+        {
+            return new spell_tournament_melee_SpellScript();
+        }
+};
+
+class spell_tournament_duel : public SpellScriptLoader
+{
+    public:
+        spell_tournament_duel() : SpellScriptLoader("spell_tournament_duel") { }
+
+        class spell_tournament_duel_SpellScript : public SpellScript
+        {
+            PrepareSpellScript(spell_tournament_duel_SpellScript);
+
+            void HandleEffectScriptEffect(SpellEffIndex /*effIndex*/)
+            {
+                if (Unit* pTarget = GetHitUnit())
+                {
+                    if (pTarget->GetTypeId() != TYPEID_PLAYER)
+                        return;
+
+                    if (Unit *caster = GetCaster()->GetCharmerOrOwner())
+                        caster->CastSpell(pTarget,62875,true);
+                }
+            }
+
+            void Register()
+            {
+                OnEffect += SpellEffectFn(spell_tournament_duel_SpellScript::HandleEffectScriptEffect, EFFECT_0, SPELL_EFFECT_SCRIPT_EFFECT);
+            }
+        };
+
+        SpellScript *GetSpellScript() const
+        {
+            return new spell_tournament_duel_SpellScript();
+        }
+};
+
 void AddSC_icecrown()
 {
     new npc_arete;
@@ -389,4 +563,8 @@ void AddSC_icecrown()
     new npc_argent_valiant;
     new npc_alorah_and_grimmin;
     new npc_guardian_pavilion;
+    new spell_tournament_charge;
+    new spell_tournament_shield;
+    new spell_tournament_melee;
+    new spell_tournament_duel;
 }
