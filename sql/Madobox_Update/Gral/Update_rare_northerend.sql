6784dcff58322d4706db1d36f7d4af249af88d17
Retriman <nicolas_katre@hotmail.com>
2011-04-06 21:10:17 -0400

    Fix Faction of rare elites of northrend

---
+++ sql/Madobox_Update/Gral/Update_rare_northerend.sql
@@ -0,0 +1,3 @@
+-- Faction of rare elites of northrend 
+UPDATE `creature_template` SET `faction_A`=14, `faction_H`=14 WHERE `entry` IN  
+(32357,32361, 32358,32481,32471,32501,32398,32377,32438,32475,32386,32400); 
