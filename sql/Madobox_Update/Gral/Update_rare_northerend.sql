51ff47f17a93f4c960e3af17f951b8ce4c6dc44c
Retriman <nicolas_katre@hotmail.com>
2011-04-06 21:43:23 -0400

    Real Fix northrend rare faction and level

--- sql/Madobox_Update/Gral/Update_rare_northerend.sql
+++ sql/Madobox_Update/Gral/Update_rare_northerend.sql
@@ -1,3 +1,34 @@
--- Faction of rare elites of northrend 
-UPDATE `creature_template` SET `faction_A`=14, `faction_H`=14 WHERE `entry` IN  
-(32357,32361, 32358,32481,32471,32501,32398,32377,32438,32475,32386,32400); 
+-- Eishorn
+UPDATE `creature_template` SET `faction_A`=16, `faction_H`=16 WHERE `entry`=32361;
+-- Aotona
+UPDATE `creature_template` SET `faction_A`=16, `faction_H`=16, `minlevel`=75, `maxlevel`=75 WHERE `entry`=32481;
+-- Tukemuth
+UPDATE `creature_template` SET `faction_A`=16, `faction_H`=16 WHERE `entry`=32400;
+-- High Thane Jorfus
+UPDATE `creature_template` SET `faction_A`=16, `faction_H`=16 WHERE `entry`=32501;
+-- Hildana Deathstealer
+UPDATE `creature_template` SET `faction_A`=16, `faction_H`=16 WHERE `entry`=32495;
+-- Old Crystalbark
+UPDATE `creature_template` SET `faction_A`=16, `faction_H`=16 WHERE `entry`=32357;
+-- Fumblub Gearwind
+UPDATE `creature_template` SET `faction_A`=16, `faction_H`=16 WHERE `entry`=32358;
+-- Perobas the Bloodthirster
+UPDATE `creature_template` SET `faction_A`=16, `faction_H`=16 WHERE `entry`=32377;
+-- Vigdis the War Maiden
+UPDATE `creature_template` SET `faction_A`=16, `faction_H`=16 WHERE `entry`=32386;
+-- King Ping
+UPDATE `creature_template` SET `faction_A`=16, `faction_H`=16 WHERE `entry`=32398;
+-- Crazed Indule Survivor
+UPDATE `creature_template` SET `faction_A`=16, `faction_H`=16 WHERE `entry`=32409;
+-- Grocklar
+UPDATE `creature_template` SET `faction_A`=16, `faction_H`=16 WHERE `entry`=32422;
+-- Seething Hate
+UPDATE `creature_template` SET `faction_A`=16, `faction_H`=16 WHERE `entry`=32429;
+-- Syreian the Bonecarver
+UPDATE `creature_template` SET `faction_A`=16, `faction_H`=16 WHERE `entry`=32438;
+-- Zuldrak Sentinel
+UPDATE `creature_template` SET `faction_A`=16, `faction_H`=16 WHERE `entry`=32447;
+-- Griegen
+UPDATE `creature_template` SET `faction_A`=16, `faction_H`=16, `minlevel`=75, `maxlevel`=75 WHERE `entry`=32471;
+-- Terror Spinner
+UPDATE `creature_template` SET `faction_A`=16, `faction_H`=16 WHERE `entry`=32475;
